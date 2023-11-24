258587b3312591624dff1d644e71ae169d7a1943
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-11-24 10:10:37 +0700

    add setters and getters

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 
 
 struct ConsoleDisplay {
+    width: usize,
+    height: usize,
     buffer: Vec<Vec<u8>>
-
 }
 
 impl ConsoleDisplay {
     pub fn with_size(width: usize, height: usize) -> Self {
         ConsoleDisplay { 
+            width,
+            height,
             buffer: Self::create_buffer(width, height)
         }
     }
@@ -25,10 +28,47 @@ impl ConsoleDisplay {
             println!("{}", String::from_utf8(line.to_vec()).unwrap());
         }
     }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn size(&self) -> (usize, usize) {
+        (self.width, self.height)
+    }
+
+    pub fn set(&mut self, pos: (usize, usize), sym: u8) -> Result<(), &str> {
+        if !self.contains(pos) {
+            return Err("Position is out of size");
+        }
+
+        self.buffer[pos.1][pos.0] = sym;
+        Ok(())
+    }
+
+    pub fn set_row(&mut self, row: usize, line: &[u8]) -> Result<(), &str> {
+        if row >= self.height {
+            return Err("Position is out of size");
+        }
+
+        self.buffer[row] = line.to_vec();
+        Ok(())
+    }
+
+    fn contains(&self, point: (usize, usize)) -> bool {
+        let (x, y) = point;
+        x < self.width && y < self.height
+    }
 }
 
 fn main() {
-    let display = ConsoleDisplay::with_size(30, 10);
+    let mut display = ConsoleDisplay::with_size(10, 3);
     display.print();
-    
+    display.set((3, 1), b'X').unwrap();
+    display.set_row(2, b"YYYYYYYYYY").unwrap();
+    display.print();
 }
